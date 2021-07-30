d9f3fde897f3faf1d278142dc296222a831da7d6
Nishi Anand <nishianand.05@gmail.com>
2021-07-30 06:45:10 +0000

    First commit

---
+++ src/FlagAnswer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import StyledButton from './StyledButton';
+import './FlagAnswer.css';
+
+const FlagAnswer = ({correct, answer, onNext}) => (
+	<div className='flag-answer'>
+		{
+			correct ? `Correct!: ${answer}` :
+			`Incorrect! Correct Answer: ${answer}`
+		}
+		
+		<StyledButton text="Next" onClick={onNext} />
+	</div>
+);
+
+export default FlagAnswer;
